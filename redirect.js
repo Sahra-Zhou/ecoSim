2922800aa194dd55eeb640ec0b45502bb37025ce
Sahra-Zhou <tzhou08@calpoly.edu>
2023-06-01 22:25:24 -0700

    redirect updates

--- redirect.js
+++ redirect.js
@@ -9,3 +9,30 @@ function toGuide(){
 function toSetup(){
     window.location.href = 'setup.html';
 }
+
+function optionHandler(id){
+    switch(id){
+        case 1:{
+            localStorage.setItem("Environment", "15000");
+            localStorage.setItem('userChoice', 'false');
+            localStorage.setItem("Species", JSON.stringify(["Wolf", "Sheep"]));
+            window.location.href = 'population.html';
+            break;
+        }
+        case 2:{
+            localStorage.setItem("Environment", "3000");
+            localStorage.setItem('userChoice', 'false');
+            localStorage.setItem("Species", JSON.stringify(["Cat", "Bird"]));
+            window.location.href = 'population.html';
+            break;
+        }
+        case 3:{
+            localStorage.setItem('userChoice', 'true');
+            window.location.href = 'userChoice.html';
+            break;
+        }
+        default: 
+        alert("Default case");
+        break;
+    }
+}
